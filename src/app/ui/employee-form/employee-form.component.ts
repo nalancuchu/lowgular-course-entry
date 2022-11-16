4b44567d93c1c50cf9e7ebc8475022bc9ec81c30
nalancuchu <nalancuchu@gmail.com>
2022-11-16 17:39:29 +0100

    feat: add new employee (form and data validation)

---
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-employee-form',
+  templateUrl: './employee-form.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EmployeeFormComponent {
+  title = "Dodaj nowego pracownika";
+  readonly employeeForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required]),
+    age: new FormControl(null, [Validators.min(0), Validators.required]),
+    salary: new FormControl(null, [Validators.min(0), Validators.required])
+  });
+}
